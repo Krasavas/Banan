c358dca88711549b0cf7a13a952687043ee364aa
test2 <astappleid@itstep.kz>
2023-02-06 15:48:38 +0600

    initial commit

---
+++ Lesson_25API/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson_25 API
+//
+//  Created by Swift on 06.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson_25API/Lesson_25_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_25_APIApp.swift
+//  Lesson_25 API
+//
+//  Created by Swift on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_25_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
